ba25a755b4f604e14b3d14efd2d2147992e65941
Pie Eip <Erreip2000@outlook.com>
2021-09-16 12:10:36 +0930

    Added some code for player ai, something like a waypoint not sure if it will work tho.

--- Assets/Scripts/PlayerAI.cs
+++ Assets/Scripts/PlayerAI.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
 public class PlayerAI : MonoBehaviour
 {
+    //Following a Vector3 array using waypoints
+
+    public GameObject[] waypoints;      //array of gameobjects
+    int current = 0;                    //number that shows which way in the array we are going to
+    float rotSpeed;                     //rotates the character
+    public float speed;                 
+    float WPradius = 1;                 //WaypointRadius basically has a value of 1 meaning it reaches a waypoint and goesd to the next one
+
+
     public Vector2 start;
     public Vector2 destination;
     public float moveSpeed;
 
-    private int x;
 
     Node[] path;
 
@@ -22,7 +31,16 @@ public class PlayerAI : MonoBehaviour
     void Update()
     {
         //how to follow Vector3 array (Vector3 = path[x].WorldPosition)
+        if(Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)  //Checking distance of current waypoint and its position and the position of the current object and if its less than WPradius
+        {
+            current++;  // adds 1 to the current value
+                if (current >= waypoints.Length) // basically checks the "[]" value therefore if it reaches its last waypoint it gets set back to 0
+                {
+                    current = 0;    
+                }
+        }
 
-        transform.position = Vector3.MoveTowards(transform.position, path[x].WorldPosition);
+        //moves the position to the current waypoint, time.delta time is used to change the speed
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
     }
 }
