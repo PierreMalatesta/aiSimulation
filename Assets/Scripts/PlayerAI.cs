67fafec99f75dce8f6b8d70c96b67f572c8e8f28
Pierre Malatesta <s210410@students.aie.edu.au>
2021-09-24 13:44:11 +0930

    pathfinding done, victory done, now i need to get a enemy state machine working.

--- Assets/Scripts/PlayerAI.cs
+++ Assets/Scripts/PlayerAI.cs
@@ -13,39 +13,50 @@ public class PlayerAI : MonoBehaviour
     public float speed;                 
     float WPradius = 0.01f;                 //WaypointRadius basically has a value of 1 meaning it reaches a waypoint and goesd to the next one
 
-    public GameObject[] Ending;
+    public GameObject Ending;
 
     public Vector2 start;
     public Vector2 destination;
+    public State currentState;
 
+    Node[] Path;
 
-    Node[] path;
+    public enum State
+    {
+        Follow,
+        Wander,
+        Flee
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         //test
+        if (currentState == State.Wander)
+        {
+            //Wander code
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (path == null)
+        if (Path == null)
         {
-            //path = Grid.FindPath(start, Grid.WorldToGridPosition(destination), Ending);
+            Path = Grid.FindPath(start, Grid.WorldToGridPosition(Ending.transform.position));
         }
         //how to follow Vector3 array (Vector3 = path[x].WorldPosition)
         
 
-        if (current < path.Length)
+        if (current < Path.Length)
         {
             //moves the position to the current waypoint, time.delta time is used to change the speed
-            transform.position = Vector3.MoveTowards(transform.position, path[current].WorldPosition(), Time.deltaTime * speed);
+            transform.position = Vector3.MoveTowards(transform.position, Path[current].WorldPosition(), Time.deltaTime * speed);
 
-            if (Vector3.Distance(path[current].WorldPosition(), transform.position) < WPradius)  //Checking distance of current waypoint and its position and the position of the current object and if its less than WPradius
+            if (Vector3.Distance(Path[current].WorldPosition(), transform.position) < WPradius)  //Checking distance of current waypoint and its position and the position of the current object and if its less than WPradius
             {
                 current++;  // adds 1 to the current value
-                if (current >= path.Length) // basically checks the "[]" value therefore if it reaches its last waypoint it gets set back to 0
+                if (current >= Path.Length) // basically checks the "[]" value therefore if it reaches its last waypoint it gets set back to 0
                 {
                     //path = Grid.FindPath(path[path.Length - 1].Position, path[0].Position);
                     //current = 0;
