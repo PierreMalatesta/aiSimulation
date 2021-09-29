3695ae90aa4af67394c5c149bf1bc1637da06172
Pie Eip <Erreip2000@outlook.com>
2021-09-29 16:18:22 +0930

    added waypoint howeve they dont do anything

---
+++ Assets/Scripts/EnemyStateMachine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStateMachine : MonoBehaviour
+{
+    public State currentState;
+
+    //easiest way to state machines
+    //do this for enemies not player.
+    public enum State
+    {
+        Follow,
+        Wander,
+        Flee
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (currentState == State.Wander)
+        {
+            //Wander code
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerAI.cs
+++ Assets/Scripts/PlayerAI.cs
@@ -17,27 +17,15 @@ public class PlayerAI : MonoBehaviour
 
     public Vector2 start;
     public Vector2 destination;
-    public State currentState;
 
     Node[] Path;
 
-   //easiest way to state machines
-   //do this for enemies not player.
-    public enum State
-    {
-        Follow,
-        Wander,
-        Flee
-    }
 
     // Start is called before the first frame update
     void Start()
     {
         //test
-        if (currentState == State.Wander)
-        {
-            //Wander code
-        }
+      
     }
 
     // Update is called once per frame
