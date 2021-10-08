b5dc84f3ee2518dc8a241ebb1b58c8f0679d95d4
Pierre Malatesta <s210410@students.aie.edu.au>
2021-10-08 14:25:27 +1100

    Done with wander state

--- Assets/Scripts/EnemyStateMachine.cs
+++ Assets/Scripts/EnemyStateMachine.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class EnemyStateMachine : MonoBehaviour
 {
     public State currentState;
+    public Transform[] wanderNodes;
+    
+    public float WPradius;
+    public float speed;
+
+    int currentWanderNode;
 
     //easiest way to state machines
     //do this for enemies not player.
@@ -18,15 +24,43 @@ public class EnemyStateMachine : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (currentState == State.Wander)
-        {
-            //Wander code
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentState == State.Wander)
+        {
+            //Wander code
+            if (currentWanderNode < wanderNodes.Length)
+            {
+                //moves the position to the current waypoint, time.delta time is used to change the speed
+                transform.position = Vector3.MoveTowards(transform.position, wanderNodes[currentWanderNode].position, Time.deltaTime * speed);
+
+                if (Vector3.Distance(wanderNodes[currentWanderNode].position, transform.position) < WPradius)  //Checking distance of current waypoint and its position and the position of the current object and if its less than WPradius
+                {
+                    currentWanderNode++;  // adds 1 to the current value
+                    if (currentWanderNode >= wanderNodes.Length) // basically checks the "[]" value therefore if it reaches its last waypoint it gets set back to 0
+                    {
+                        //path = Grid.FindPath(path[path.Length - 1].Position, path[0].Position);
+                        currentWanderNode = 0;
+                    }
+                }
+
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //change state to follow
+            //get grid position of this enemy use Grid.WorldToGridPosition()
+            //get grid position of player
+            //pathfind Grid.PathFind()
+        }
     }
 }
